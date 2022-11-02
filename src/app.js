62fbd3b46de066c63a67000a4b3f098e81f01289
Pragmatic <cryptopragmatic@protonmail.com>
2022-11-02 06:32:09 +0100

    animation work

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ import 'bootstrap'
 import "bootswatch/dist/slate/bootstrap.min.css";
 import '@fortawesome/fontawesome-free/css/all.css'
 import anime from 'animejs/lib/anime.es.js';
+import $ from "jquery";
 
 window.Alpine = Alpine
 Alpine.start();
 
 const wrapperEl = document.querySelector('.wrapper');
-const numberOfEls = 90;
+const numberOfEls = 60;
 const duration = 6000;
 const delay = duration / numberOfEls;
 
@@ -22,17 +23,13 @@ function createEl(i) {
   let el = document.createElement('div');
   const rotate = (360 / numberOfEls) * i;
   const translateY = -50;
-  const hue = Math.round(360 / numberOfEls * i);
   el.classList.add('el');
-//   el.style.backgroundColor = 'hsl(' + hue + ', 40%, 60%)';
-  el.style.backgroundColor = '#DC4040';
+  el.classList.add('red');
   el.style.transform = 'rotate(' + rotate + 'deg) translateY(' + translateY + '%)';
   tl.add({
     begin: function() {
       anime({
         targets: el,
-        // backgroundColor: ['hsl(' + hue + ', 40%, 60%)', 'hsl(' + hue + ', 60%, 80%)'],
-        backgroundColor: '#DC4040',
         rotate: [rotate + 'deg', rotate + 10 +'deg'],
         translateY: [translateY + '%', translateY + 10 + '%'],
         scale: [1, 1.25],
@@ -46,3 +43,18 @@ function createEl(i) {
 };
 
 for (let i = 0; i < numberOfEls; i++) createEl(i);
+
+tl.pause();
+
+$("#changeColor").on("click", function() {
+    $(this).blur();
+    if ($(".el").hasClass("red")) {
+        tl.play();
+        $(".el").removeClass("red");
+        $(".el").addClass("green");
+    } else {
+        tl.pause();
+        $(".el").removeClass("green");
+        $(".el").addClass("red");
+    }
+});
